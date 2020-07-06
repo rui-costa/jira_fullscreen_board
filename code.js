c74eb289a53b06f7fc4efd0f442c35daee3fd532
Rui Costa <44004884+rui-costa@users.noreply.github.com>
2020-07-06 13:10:54 +0200

    NEW : Mutation tracking
    Tracking changes when JIRA bar is used to navigate to other views, apply the button if not present

--- code.js
+++ code.js
@@ -1,55 +1,94 @@
-var fullscreen = false;
-var h_board = "";
+const version  = '0.20.0706'
 
-var link = document.createElement( "link" );
-link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
-link.type = "text/css";
-link.rel = "stylesheet";
-link.media = "screen,print";
+const buttonFS = function () {
 
-document.getElementsByTagName( "head" )[0].appendChild( link );
+    this.element = document.createElement('BUTTON')
+    this.element.setAttribute('id', 'button_fs')
+    this.element.setAttribute('class', 'aui-button aui-button-primary')
+    this.element.style.float = 'left'
+    this.element.style.margin = '4px 8px 0px 0px'
+    this.element.innerHTML = '<span class="material-icons">settings_overscan</span>'
+    this.element.addEventListener('click', toogle)
 
-function init(){
+    this.getElement = function () {
+        return this.element
+    }
 
-    var main = document.getElementById("ghx-quick-filters").childNodes[0];
-    if(main === null || main == undefined ){
-        ms = 500
-        console.log("JIRA FULLSCREEN : Not loaded yet, trying in "+ms+" mileseconds");
-        setTimeout(init,ms);
-    }else{
-        h_board = document.getElementById("ghx-work").style.height;
-        var button_fs = document.createElement("BUTTON"); 
-        button_fs.innerHTML = '<span class="material-icons">settings_overscan</span>';
-        button_fs.setAttribute("id", "button_fs");
-        button_fs.setAttribute("class", "aui-button aui-button-primary");
-        button_fs.setAttribute("title", "Fullscreen");
-        main.insertBefore(button_fs,main.childNodes[0]);
-        document.getElementById("button_fs").addEventListener("click", toogle_fs);
-        document.getElementById("button_fs").style.marginRight = "10px";
+    this.clicked = false
+
+    this.toogle = function () {
+        if (this.clicked) {
+            '<span class="material-icons">settings_overscan</span>'
+            this.clicked = false
+        } else {
+            '<span class="material-icons">cancel_presentation</span>'
+            this.clicked = true
+        }
+
     }
 }
 
-function toogle_fs(){
-    if(fullscreen){
-        document.getElementById("ghx-header").style.display = "";
-        document.getElementById("navigation-app").style.display = "";
-        document.getElementById("content").style.margin = "";
-        document.getElementById("content").style.borderTop = "";
-        document.getElementById("ghx-quick-filters").style.marginBottom = "";
-        document.getElementById("ghx-work").style.height = h_board;
-        button_fs.innerHTML = '<span class="material-icons">settings_overscan</span>';
-        
+const link = document.createElement('link')
+
+let button = new buttonFS()
+let hBoard = ''
+
+function init() {
+    fullscreen = false
+
+    let main = document.querySelector('#ghx-quick-filters')
+
+    if (main === null || main == undefined) {
+        // try again in 500ms
+        let ms = 1000
+        setTimeout(init, ms)
+    } else {
+
+        link.href = 'https://fonts.googleapis.com/icon?family=Material+Icons'
+        link.type = 'text/css'
+        link.rel = 'stylesheet'
+        link.media = 'screen,print'
+        document.getElementsByTagName('head')[0].appendChild(link)
+
+        console.log(`FSAB4JIRA : Full-Screen agile board mode for Jira`)
+        console.log(`FSAB4JIRA : Loading version ${version}`)
+
+        //Observe for modifications of content when navigating pages
+        const observer = new MutationObserver(function () {
+
+            if (document.querySelector('#button_fs') === null || document.querySelector('#button_fs') === undefined) {
+                main.insertBefore(button.getElement(), main.childNodes[0])
+            }
+        })
+        observer.observe(main, { subtree: true, childList: true })
+
+        // initiliaze
+        hBoard = document.getElementById('ghx-work').style.height
+
+    }
+}
+
+function toogle() {
+
+    if (button.clicked) {
+        document.getElementById('ghx-header').style.display = ''
+        document.getElementById('navigation-app').style.display = ''
+        document.getElementById('content').style.margin = ''
+        document.getElementById('content').style.borderTop = ''
+        document.getElementById('ghx-quick-filters').style.marginBottom = ''
+        document.getElementById('ghx-work').style.height = hBoard
         fullscreen = false
-    }else{
-        document.getElementById("ghx-header").style.display = "none";
-        document.getElementById("navigation-app").style.display = "none";
-        document.getElementById("content").style.margin = "2px";
-        document.getElementById("content").style.borderTop = "2px";
-        document.getElementById("ghx-quick-filters").style.marginBottom = "0px";
-        document.getElementById("ghx-work").style.height = window.innerHeight + "px";
-        button_fs.innerHTML = '<span class="material-icons">cancel_presentation</span>';
-        fullscreen = true;
+    } else {
+        document.getElementById('ghx-header').style.display = 'none'
+        document.getElementById('navigation-app').style.display = 'none'
+        document.getElementById('content').style.margin = '2px'
+        document.getElementById('content').style.borderTop = '2px'
+        document.getElementById('ghx-quick-filters').style.marginBottom = '0px'
+        document.getElementById('ghx-work').style.height = window.innerHeight + 'px'
+        fullscreen = true
     }
+
+    button.toogle()
 }
 
-init();
+init()
